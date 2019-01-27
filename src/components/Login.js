b308e457e25abf8acf7a4910800a5c9ba912a534
Rahulio <rahul.ramesh888@gmail.com>
2019-01-27 17:57:13 -0500

    Login and logout functionality implemented

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+
 import { AUTH_TOKEN } from "../constants";
 
-const Login = () => {
+const Login = props => {
   const [login, setLogin] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+
+  const _confirm = async data => {
+    const { token } = login ? data.login : data.signup;
+    _saveUserData({ token });
+    props.history.push("/");
+  };
 
   return (
     <>
@@ -32,10 +41,21 @@ const Login = () => {
           placeholder="Your password here"
         />
       </div>
+
       <div className="flex mt3">
-        <div className="pointer mr2 button" onClick={() => _confirm()}>
-          {login ? "Login" : "Create account"}
-        </div>
+        <Mutation
+          mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+          variables={{ email, password, name }}
+          onCompleted={_confirm}
+          onError={alert}
+        >
+          {mutation => (
+            <div className="pointer mr2 button" onClick={mutation}>
+              {login ? "Login" : "Create account"}
+            </div>
+          )}
+        </Mutation>
+
         <div className="pointer button" onClick={() => setLogin(!login)}>
           {login ? "Need to create an account?" : "Already have an account?"}
         </div>
@@ -44,10 +64,24 @@ const Login = () => {
   );
 };
 
-const _confirm = async () => {
-  //..Something here
-};
-
-const _saveUserData = token => localStorage.setItem(AUTH_TOKEN, token);
-
 export default Login;
+
+//Store JWT in localStorage just for the purposes of this GraphQL project.
+//Wouldn't do it otherwise 💩
+const _saveUserData = ({ token }) => localStorage.setItem(AUTH_TOKEN, token);
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
