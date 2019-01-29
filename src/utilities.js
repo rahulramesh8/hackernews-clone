010d5e5b8a4cc4ff0c5f26a7e7978244f73ebbb5
Rahulio <rahul.ramesh888@gmail.com>
2019-01-28 21:25:23 -0500

    Changes

--- src/utilities.js
+++ src/utilities.js
@@ -14,9 +14,9 @@ const timeDifference = (current, previous) => {
   if (elapsed < milliSecondsPerMinute) {
     return "less than 1 min ago";
   } else if (elapsed < milliSecondsPerHour) {
-    return Math.round(elapsed / milliSecondsPerMinute) + " min ago";
+    return Math.round(elapsed / milliSecondsPerMinute) + "min ago";
   } else if (elapsed < milliSecondsPerDay) {
-    return Math.round(elapsed / milliSecondsPerHour) + " h ago";
+    return Math.round(elapsed / milliSecondsPerHour) + "h ago";
   } else if (elapsed < milliSecondsPerMonth) {
     return Math.round(elapsed / milliSecondsPerDay) + " days ago";
   } else if (elapsed < milliSecondsPerYear) {
